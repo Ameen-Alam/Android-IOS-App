d3de68c25d3ed3fb207441b9cbd124f55dd9056c
AxiomInnovationLab <khanadilaltaf@gmail.com>
2020-03-18 18:03:28 +0500

    set-up project architecture

---
+++ App/Config/index.dev.js
@@ -0,0 +1,28 @@
+export const Config = {
+  API_URL: 'https://jsonplaceholder.typicode.com/users/',
+}
+
+
+// [
+//   {
+//     "id": 1,
+//     "name": "Ameen Alam",
+//     "username": "Ameen",
+//     "email": "ameenalam202@gmail.com",
+//     "address": {
+//       "street": "Karachi Pakistan",
+//       "city": "Karachi",
+//       "geo": {
+//         "lat": "24.8615088",
+//         "lng": "67.0733536"
+//       }
+//     },
+//     "phone": "+92 304 386 6719",
+//     "website": "ameenalam.com",
+//     "company": {
+//       "name": "Panacloud",
+//       "catchPhrase": "Software House",
+//       "bs": "Innovation"
+//     }
+//   }
+// ]
---
+++ App/Config/index.staging.js
@@ -0,0 +1,3 @@
+export const Config = {
+  API_URL: 'https://api.jsonbin.io/b/5e70ebf5e5531f68eea1cc82',
+}
